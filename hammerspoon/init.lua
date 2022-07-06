0e6e13a0b20501f4e43e57e45ff3d50b2e1ada29
meiraka <meiraka.pre@gmail.com>
2022-07-06 15:50:58 +0900

    kitty macos

---
+++ hammerspoon/init.lua
@@ -0,0 +1,10 @@
+hs.hotkey.bind({"cmd"}, "space", function()
+    local kitty = hs.application.get("kitty")
+    if kitty == nil then
+        hs.application.launchOrFocus("/Applications/kitty.app")
+    elseif kitty:isFrontmost() then
+        kitty:hide()
+    else
+        hs.application.launchOrFocus("/Applications/kitty.app")
+    end
+end)
