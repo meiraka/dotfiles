e472eb0ab156ad6fb672c8f22c853299da6d0b00
meiraka <meiraka.pre@gmail.com>
2023-01-22 23:50:49 +0900

    config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -15,58 +15,67 @@ return require('packer').startup(function(use)
     use { 'wbthomason/packer.nvim' }
     use { 'junegunn/fzf', run = ':call fzf#install()' }
     use { 'junegunn/fzf.vim' }
-    use { 'neovim/nvim-lspconfig' }
-    use { 'williamboman/mason.nvim' }
-    use { 'williamboman/mason-lspconfig.nvim' }
-    use { 'hrsh7th/nvim-cmp' }
-    use { 'hrsh7th/cmp-nvim-lsp' }
-    use { 'ellisonleao/gruvbox.nvim' }
+    use { 'mattn/vim-goimports' }
+    use { 'neovim/nvim-lspconfig',
+        requires = {
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+        },
+        config = function()
+            require('mason').setup()
+            require('mason-lspconfig').setup_handlers({ function(server)
+                local opts = {
+                    capabilities = require('cmp_nvim_lsp').default_capabilities(),
+                    settings = {
+                        -- suppress neovim's undefined global `vim`
+                        Lua = { diagnostics = { globals = { 'vim' } } },
+                    },
+                }
+                require('lspconfig')[server].setup(opts)
+            end
+            })
+            local cmp = require("cmp");
+            cmp.setup({
+                sources = { { name = "nvim_lsp" } },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-p>"] = cmp.mapping.select_prev_item(),
+                    ["<C-n>"] = cmp.mapping.select_next_item(),
+                    ['<C-l>'] = cmp.mapping.complete(),
+                    ['<C-e>'] = cmp.mapping.abort(),
+                    ["<CR>"] = cmp.mapping.confirm { select = true },
+                }),
+            })
+        end
+    }
+    use { 'ellisonleao/gruvbox.nvim', config = function() vim.cmd('colorscheme gruvbox') end }
     use { 'nvim-treesitter/nvim-treesitter',
         run = function()
             local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
             ts_update()
         end,
+        config = function()
+            require('nvim-treesitter.configs').setup({
+                ensure_installed = { 'go', 'css', 'yaml', 'regex', 'bash', 'markdown', 'markdown_inline' },
+                highlight = {
+                    enable = true,
+                    disable = { 'lua', 'vim', 'javascript' },
+                },
+            })
+        end
     }
-    use { 'akinsho/toggleterm.nvim', tag = '*' }
+    use { 'akinsho/toggleterm.nvim', tag = '*', config = function() require("toggleterm").setup() end }
     use({ "folke/noice.nvim",
-        config = function() require("noice").setup({}) end,
         requires = {
             "MunifTanjim/nui.nvim",
             "rcarriga/nvim-notify",
-        }
+        },
+        config = function()
+            require("noice").setup({})
+        end,
     })
 
-    require("toggleterm").setup()
-    require('mason').setup()
-    require('mason-lspconfig').setup_handlers({ function(server)
-        local opts = {
-            capabilities = require('cmp_nvim_lsp').default_capabilities(),
-            settings = {
-                -- suppress neovim's undefined global `vim`
-                Lua = { diagnostics = { globals = { 'vim' } } },
-            },
-        }
-        require('lspconfig')[server].setup(opts)
-    end
-    })
-    require('nvim-treesitter.configs').setup({
-        ensure_installed = { 'go', 'css', 'yaml', 'regex', 'bash', 'markdown', 'markdown_inline' },
-        highlight = {
-            enable = true,
-            disable = { 'lua', 'javascript' },
-        },
-    })
-    local cmp = require("cmp");
-    cmp.setup({
-        sources = { { name = "nvim_lsp" } },
-        mapping = cmp.mapping.preset.insert({
-            ["<C-p>"] = cmp.mapping.select_prev_item(),
-            ["<C-n>"] = cmp.mapping.select_next_item(),
-            ['<C-l>'] = cmp.mapping.complete(),
-            ['<C-e>'] = cmp.mapping.abort(),
-            ["<CR>"] = cmp.mapping.confirm { select = true },
-        }),
-    })
     vim.g.mapleader = " "
     vim.keymap.set('n', '<leader>h', vim.lsp.buf.hover)
     vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename)
@@ -77,7 +86,6 @@ return require('packer').startup(function(use)
     vim.keymap.set('n', 'fb', '<cmd>Buffers<cr>')
     vim.opt.mouse = nil
     vim.cmd([[
-colorscheme gruvbox
 command LspDefinition lua vim.lsp.buf.definition()
 command LspFormat lua vim.lsp.buf.format { async = true }
 command LspReferences lua vim.lsp.buf.references()
