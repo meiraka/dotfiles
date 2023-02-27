055042bd3788636f5f3c0b9c95818562577bba7b
meiraka <meiraka.pre@gmail.com>
2023-02-27 23:39:04 +0900

    try fancy tab bar

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -4,7 +4,11 @@ local mux = wezterm.mux
 local color = require("color")
 
 local myColors = wezterm.color.get_builtin_schemes()["Gruvbox dark, medium (base16)"]
-myColors.tab_bar = { background = color.alpha(myColors.background, 0) }
+local inactive = color.pseudo_alpha(myColors.foreground, myColors.background, 0.3)
+myColors.tab_bar = {
+    background = color.alpha(myColors.background, 0),
+    inactive_tab_edge = inactive,
+}
 local myWorkspaces = 8
 
 local myLeader = { key = 'f', mods = 'CTRL', timeout_milliseconds = 1000 }
@@ -34,7 +38,6 @@ wezterm.on("gui-startup", function()
     mux.set_active_workspace("1")
 end)
 
-local inactive = color.pseudo_alpha(myColors.foreground, myColors.background, 0.3)
 wezterm.on('update-status', function(window, _)
     -- workspace indicator
     local wsExists = {}
@@ -60,7 +63,15 @@ wezterm.on('update-status', function(window, _)
     table.insert(fmt, { Text = " " })
 
     window:set_left_status(wezterm.format(fmt))
+    if wezterm.target_triple ~= "x86_64-unknown-linux-gnu" then
+        window:set_right_status(wezterm.format({
+            { Text = wezterm.strftime("%a %b %d %Y %H:%M:%S") },
+        }))
+    else
+        window:set_right_status("")
+    end
 end)
+
 
 return {
     audible_bell = "Disabled",
@@ -75,11 +86,18 @@ return {
     leader = myLeader,
     keys = myKeys,
     show_new_tab_button_in_tab_bar = false,
+    show_tabs_in_tab_bar = false,
     text_background_opacity = 1,
-    tab_max_width = 0, -- disable tab
-    use_fancy_tab_bar = false,
+    use_fancy_tab_bar = true,
     window_background_opacity = 1,
     window_decorations = "NONE",
+    window_frame = {
+        font = wezterm.font_with_fallback({
+            "HackGen Console NFJ",
+        }),
+        font_size = 12.0,
+        active_titlebar_bg = myColors.background,
+    },
     window_padding = { left = '0.5cell', right = 0, top = '0.25cell', bottom = 0 },
     inactive_pane_hsb = {
         saturation = 1.0,
