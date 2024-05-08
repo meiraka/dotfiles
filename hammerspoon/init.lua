77f8dc1bcb698492b039f48a7cf446a3b89e39a2
meiraka <meiraka.pre@gmail.com>
2024-05-08 18:49:14 +0900

    term center

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,18 +1,31 @@
-spaces = require('hs.spaces')
-hs.hotkey.bind({"cmd"}, "space", function()
-    local term = hs.application.get("WezTerm")
-    if term == nil then
-        hs.application.launchOrFocus("/Applications/WezTerm.app")
-        local term = hs.application.get("WezTerm")
-        term:mainWindow():setFullScreen(true)
-    elseif term:isFrontmost() then
-        term:hide()
-    else
-        -- move window to current space
-        local sp = spaces.activeSpaceOnScreen()
-        local win = term:mainWindow()
-        spaces.moveWindowToSpace(win:id(), sp)
-        term:mainWindow():setFullScreen(true)
-        hs.application.launchOrFocus("/Applications/WezTerm.app")
+local application = require('hs.application')
+local geometry = require('hs.geometry')
+local hotkey = require('hs.hotkey')
+local spaces = require('hs.spaces')
+local window = require('hs.window')
+
+local function toggle(appName)
+    return function()
+        local app = application.get(appName)
+        if app == nil then
+            app = application.open("WezTerm")
+        end
+        if app:isFrontmost() then
+            app:hide()
+        else
+            local desktopSize = window.desktop():size()
+            app:mainWindow():setSize(geometry.size(desktopSize.w * 18 / 20, desktopSize.h * 18 / 20))
+            app:mainWindow():centerOnScreen()
+            app:mainWindow():setSize(geometry.size(desktopSize.w * 18 / 20, desktopSize.h * 18 / 20))
+            app:mainWindow():centerOnScreen()
+            app:mainWindow():setSize(geometry.size(desktopSize.w * 18 / 20, desktopSize.h * 18 / 20))
+            app:mainWindow():centerOnScreen()
+            local sp = spaces.activeSpaceOnScreen()
+            local win = app:mainWindow()
+            spaces.moveWindowToSpace(win:id(), sp)
+        end
     end
-end)
+end
+
+
+hotkey.bind({ "cmd" }, "space", toggle('WezTerm'))
