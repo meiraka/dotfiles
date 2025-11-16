8935140084a4153fc99c86a25c2944797aebd59f
meiraka <meiraka.pre@gmail.com>
2025-11-16 16:44:21 +0900

    rename desc

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -124,12 +124,12 @@ return {
         },
         cmd = "Trouble",
         keys = {
-            { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>",              desc = "Diagnostics (Trouble)", },
-            { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)", },
-            { "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>",      desc = "Symbols (Trouble)", },
-            { "<leader>h",  "<cmd>Trouble lsp toggle<cr>",                      desc = "LSP Definitions / references / ... (Trouble)", },
-            { "<leader>xl", "<cmd>Trouble loclist toggle<cr>",                  desc = "Location List (Trouble)", },
-            { "<leader>xq", "<cmd>Trouble qflist toggle<cr>",                   desc = "Quickfix List (Trouble)", },
+            { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>",              desc = "List Diagnostics", },
+            { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "List Diagnostics/Buffer", },
+            { "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>",      desc = "List Symbols", },
+            { "<leader>h",  "<cmd>Trouble lsp toggle<cr>",                      desc = "List LSP References", },
+            { "<leader>xl", "<cmd>Trouble loclist toggle<cr>",                  desc = "Location List", },
+            { "<leader>xq", "<cmd>Trouble qflist toggle<cr>",                   desc = "Quickfix List", },
         },
     },
 }
