a6ca54d0b4a59259a7990f3b202577200b92b4f4
meiraka <meiraka.pre@gmail.com>
2025-11-29 16:50:50 +0900

    add border to LSP hover

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -10,8 +10,9 @@ return {
             'hrsh7th/vim-vsnip',
         },
         keys = {
-            { '<leader>r', vim.lsp.buf.rename,     desc = 'LSP Rename' },
-            { '<leader>d', vim.lsp.buf.definition, desc = 'LSP Definition' },
+            { '<leader>r', vim.lsp.buf.rename,                                       desc = 'LSP Rename' },
+            { '<leader>d', vim.lsp.buf.definition,                                   desc = 'LSP Definition' },
+            { 'K',         function() vim.lsp.buf.hover({ border = 'rounded' }) end, desc = 'LSP Hover' },
         },
         event = { "BufReadPre", "BufNewFile" },
         config = function()
