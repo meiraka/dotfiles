df4583cbf3756a0a437fba5c46d53dbfcdd24c49
meiraka <meiraka.pre@gmail.com>
2025-11-29 16:44:48 +0900

    auto fmt

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -31,7 +31,22 @@ return {
                 capabilities = require('cmp_nvim_lsp').default_capabilities(),
                 filetypes = { 'haskell', 'lhaskell', 'cabal' },
             })
-            vim.lsp.enable({"hls"})
+            vim.lsp.enable({ "hls" })
+            vim.api.nvim_create_autocmd('LspAttach', {
+                group = vim.api.nvim_create_augroup('my.lsp', {}),
+                callback = function(args)
+                    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+                    if not client:supports_method('textDocument/willSaveWaitUntil') and client:supports_method('textDocument/formatting') then
+                        vim.api.nvim_create_autocmd('BufWritePre', {
+                            group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
+                            buffer = args.buf,
+                            callback = function()
+                                vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+                            end,
+                        })
+                    end
+                end,
+            })
             local cmp = require("cmp");
             cmp.setup({
                 snippet = { expand = function(args) vim.fn["vsnip#anonymous"](args.body) end },
