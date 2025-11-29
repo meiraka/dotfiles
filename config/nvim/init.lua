f6b57d4db13ec80442fdf41f8c7c4f873a78af77
meiraka <meiraka.pre@gmail.com>
2025-11-29 12:10:04 +0900

    remove vimscript block

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -13,41 +13,29 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 require("lazy").setup("plugins")
 
-vim.api.nvim_create_autocmd("QuickFixCmdPost", {
-    callback = function()
-        vim.cmd([[Trouble qflist open]])
-    end,
-})
+vim.opt.mouse = ""
+vim.opt.wildmode = "list:longest,full"
+vim.opt.termguicolors = true
+vim.opt.foldmethod = "indent"
+vim.opt.foldenable = false
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.backspace = "start,eol,indent"
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
 
 vim.diagnostic.config({
-  signs = {
-    text = {
-      [vim.diagnostic.severity.ERROR] = "󰅚",
-      [vim.diagnostic.severity.WARN] = "󰀪",
-      [vim.diagnostic.severity.INFO] = "",
-      [vim.diagnostic.severity.HINT] = "󰌶",
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = "󰅚",
+            [vim.diagnostic.severity.WARN] = "󰀪",
+            [vim.diagnostic.severity.INFO] = "",
+            [vim.diagnostic.severity.HINT] = "󰌶",
+        },
     },
-  },
 })
 
 vim.keymap.set('n', '<leader>b', '<cmd>b#<cr>')
-
-vim.opt.mouse = ""
-vim.cmd([[
-command LspDefinition lua vim.lsp.buf.definition()
-command LspFormat lua vim.lsp.buf.format { async = true }
-command LspReferences lua vim.lsp.buf.references()
-command LspCodeAction lua vim.lsp.buf.code_action()
-set wildmode=list:longest,full
-set termguicolors
-set foldmethod=indent
-set nofoldenable
-set number
-set cursorline
-set hlsearch
-set incsearch
-set backspace=start,eol,indent
-set tabstop=4
-set shiftwidth=4
-set expandtab
-    ]])
