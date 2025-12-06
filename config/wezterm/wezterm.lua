1459333ad62701078599d69d7383dd8efc3d74d1
meiraka <meiraka.pre@gmail.com>
2025-12-06 23:41:59 +0900

    use config_builder

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -3,18 +3,27 @@ local act = wezterm.action
 local mux = wezterm.mux
 local color = require("color")
 local powerline = require("powerline")
-local opacity = 0.95
+local config = wezterm.config_builder()
 
-local myFont = wezterm.font("PlemolJP35 Console NF", { weight = "Medium", style = "Normal" })
-local myColors = wezterm.color.get_builtin_schemes()["Gruvbox Material (Gogh)"]
-local inactive = color.pseudo_alpha(myColors.foreground, myColors.background, 0.3)
-myColors.tab_bar = {
-    background = color.alpha(myColors.background, opacity),
-    inactive_tab_edge = inactive,
+config:set_strict_mode(true)
+
+config.font = wezterm.font("PlemolJP35 Console NF", { weight = "Medium", style = "Normal" })
+config.font_rules = { { italic = true, font = config.font } }
+config.font_size = 12
+config.dpi = 192
+
+config.text_background_opacity = 1
+config.window_background_opacity = 0.95
+config.macos_window_background_blur = 20
+config.colors = wezterm.color.get_builtin_schemes()["Gruvbox Material (Gogh)"]
+config.colors.tab_bar = {
+    background = color.alpha(config.colors.background, config.window_background_opacity),
+    inactive_tab_edge = color.pseudo_alpha(config.colors.foreground, config.colors.background, 0.3),
 }
-local myWorkspaces = 8
-local myLeader = { key = 'f', mods = 'CTRL', timeout_milliseconds = 1000 }
-local myKeys = {
+
+config.disable_default_key_bindings = true
+config.leader = { key = 'f', mods = 'CTRL', timeout_milliseconds = 1000 }
+config.keys = {
     { key = "L",     mods = "CTRL|SHIFT", action = act.ShowDebugOverlay },
     { key = "w",     mods = "SUPER",      action = act.CloseCurrentTab({ confirm = true }) },
     { key = "c",     mods = "SUPER",      action = act.CopyTo("ClipboardAndPrimarySelection") },
@@ -28,8 +37,25 @@ local myKeys = {
     { key = "f",     mods = "LEADER",     action = act.ToggleFullScreen },
     { key = "t",     mods = "ALT",        action = act.EmitEvent("toggle-tabbar") },
 }
-for i = 1, myWorkspaces do
-    table.insert(myKeys, {
+
+
+config.window_decorations = "NONE"
+config.window_padding = { left = '0.5cell', right = 0, top = '0.25cell', bottom = 0 }
+config.enable_tab_bar = false
+config.show_new_tab_button_in_tab_bar = false
+config.show_tabs_in_tab_bar = false
+config.use_fancy_tab_bar = false
+config.inactive_pane_hsb = {
+    saturation = 1.0,
+    brightness = 1.0,
+}
+config.audible_bell = "Disabled"
+config.use_ime = true
+
+-- workspaces
+local workspaces = 8
+for i = 1, workspaces do
+    table.insert(config.keys, {
         key = tostring(i),
         mods = 'ALT',
         action = act.SwitchToWorkspace({
@@ -56,20 +82,20 @@ wezterm.on('update-status', function(window, pane)
     local wsExists = {}
     for _, l in ipairs(mux.get_workspace_names()) do wsExists[l] = true end
     local fmt = {}
-    table.insert(fmt, { Background = { Color = myColors.tab_bar.background } })
-    for i = 1, myWorkspaces do
+    table.insert(fmt, { Background = { Color = config.colors.tab_bar.background } })
+    for i = 1, workspaces do
         local ws = tostring(i)
         if ws == window:active_workspace() then
             -- active workspace
-            table.insert(fmt, { Foreground = { Color = myColors.foreground } })
+            table.insert(fmt, { Foreground = { Color = config.colors.foreground } })
             table.insert(fmt, { Text = " ●" })
         elseif wsExists[ws] then
             -- inactive workspaces
-            table.insert(fmt, { Foreground = { Color = inactive } })
+            table.insert(fmt, { Foreground = { Color = config.colors.tab_bar.inactive_tab_edge } })
             table.insert(fmt, { Text = " ●" })
         else
             -- empty workspaces
-            table.insert(fmt, { Foreground = { Color = inactive } })
+            table.insert(fmt, { Foreground = { Color = config.colors.tab_bar.inactive_tab_edge } })
             table.insert(fmt, { Text = " ○" })
         end
     end
@@ -79,24 +105,24 @@ wezterm.on('update-status', function(window, pane)
     local right = {}
 
     -- kube context from zshprompt
-    local kube_color = myColors.ansi[5]
+    local kube_color = config.colors.ansi[5]
     local context = pane:get_user_vars().KUBE_CONTEXT
     if context ~= nil and context ~= "" then
         powerline.right_hard(right, kube_color)
-        table.insert(right, { Foreground = { Color = myColors.background } })
+        table.insert(right, { Foreground = { Color = config.colors.background } })
         table.insert(right, { Text = '󱃾' .. ' ' })
         table.insert(right, { Text = context .. ' ' })
         local namespace = pane:get_user_vars().KUBE_NAMESPACE
         if namespace ~= nil and namespace ~= "" then
-            powerline.right_soft(right, myColors.background)
+            powerline.right_soft(right, config.colors.background)
             table.insert(right, { Text = namespace .. ' ' })
         end
     end
 
     -- clock
     if window:get_dimensions().is_full_screen then
-        powerline.right_hard(right, color.pseudo_alpha(myColors.background, myColors.foreground, 0.2))
-        table.insert(right, { Foreground = { Color = myColors.background } })
+        powerline.right_hard(right, color.pseudo_alpha(config.colors.background, config.colors.foreground, 0.2))
+        table.insert(right, { Foreground = { Color = config.colors.background } })
         table.insert(right, { Text = wezterm.strftime('%a %b %d %Y %H:%M:%S') .. ' ' })
     end
     window:set_right_status(wezterm.format(right))
@@ -112,36 +138,4 @@ wezterm.on("toggle-tabbar", function(window, _)
     window:set_config_overrides(overrides)
 end)
 
-local font_size = 12
-
-return {
-    audible_bell = "Disabled",
-    enable_tab_bar = false,
-    colors = myColors,
-    font = myFont,
-    font_size = font_size,
-    font_rules = { { italic = true, font = myFont } },
-    dpi = 192,
-    disable_default_key_bindings = true,
-    leader = myLeader,
-    macos_window_background_blur = 20,
-    keys = myKeys,
-    show_new_tab_button_in_tab_bar = false,
-    show_tabs_in_tab_bar = false,
-    text_background_opacity = 1,
-    use_fancy_tab_bar = false,
-    use_ime = true,
-    window_background_opacity = opacity,
-    window_decorations = "NONE",
-    window_frame = {
-        font = myFont,
-        font_size = font_size,
-        active_titlebar_bg = myColors.background,
-        inactive_titlebar_bg = myColors.background,
-    },
-    window_padding = { left = '0.5cell', right = 0, top = '0.25cell', bottom = 0 },
-    inactive_pane_hsb = {
-        saturation = 1.0,
-        brightness = 1.0,
-    },
-}
+return config
