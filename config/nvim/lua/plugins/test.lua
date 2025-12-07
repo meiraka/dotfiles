60551687be39e808647c3b274fd0ca056a1d93ea
meiraka <meiraka.pre@gmail.com>
2025-12-07 19:33:11 +0900

    add neotest

--- config/nvim/lua/plugins/test.lua
+++ config/nvim/lua/plugins/test.lua
@@ -18,4 +18,29 @@ return {
             { '<leader>t', '<cmd>TestNearest<cr>', desc = 'Run Test' },
         },
     },
+    {
+        "nvim-neotest/neotest",
+        dependencies = {
+            "nvim-neotest/nvim-nio",
+            "nvim-lua/plenary.nvim",
+            "antoinemadec/FixCursorHold.nvim",
+            {
+                "fredrikaverpil/neotest-golang",
+                version = "*",                                                              -- Optional, but recommended; track releases
+                build = function()
+                    vim.system({ "go", "install", "gotest.tools/gotestsum@latest" }):wait() -- Optional, but recommended
+                end,
+            },
+        },
+        config = function()
+            local config = {
+                -- runner = "gotestsum", -- Optional, but recommended
+            }
+            require("neotest").setup({
+                adapters = {
+                    require("neotest-golang")(config),
+                },
+            })
+        end,
+    },
 }
