57f0b52d4f6c1dbec126041c98a8048f970f99e5
meiraka <meiraka.pre@gmail.com>
2025-12-07 19:32:06 +0900

    use main branch treesitter

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,17 +1,47 @@
 return {
+    -- {
+    --     'nvim-treesitter/nvim-treesitter',
+    --     version = '*',
+    --     build = ":TSUpdate",
+    --     config = function()
+    --         require('nvim-treesitter.configs').setup({
+    --             ensure_installed = { 'go', 'css', 'yaml', 'regex', 'bash', 'markdown', 'markdown_inline' },
+    --             sync_install = false,
+    --             highlight = {
+    --                 enable = true,
+    --                 disable = { 'lua', 'vim', 'javascript' },
+    --             },
+    --         })
+    --     end,
+    -- },
     {
         'nvim-treesitter/nvim-treesitter',
-        version = '*',
+        branch = "main",
         build = ":TSUpdate",
-        config = function()
-            require('nvim-treesitter.configs').setup({
-                ensure_installed = { 'go', 'css', 'yaml', 'regex', 'bash', 'markdown', 'markdown_inline' },
-                sync_install = false,
-                highlight = {
-                    enable = true,
-                    disable = { 'lua', 'vim', 'javascript' },
-                },
+        opts = {
+            install_dir = vim.fn.stdpath("data") .. "/lazy/nvim-treesitter",
+            ensure_installed = { 'go', 'css', 'yaml', 'regex', 'bash', 'markdown', 'markdown_inline' },
+        },
+        config = function(_, opts)
+            require("nvim-treesitter").setup(opts)
+            if vim.fn.executable('tree-sitter') == 1 then -- https://github.com/nvim-treesitter/nvim-treesitter/issues/8010
+                require("nvim-treesitter").install(opts.ensure_installed)
+            else
+                vim.notify("nvim-treesitter: tree-sitter cli is not installed", vim.log.levels.WARN,
+                    { title = "Setup plugins" })
+            end
+            vim.api.nvim_create_autocmd('FileType', {
+                pattern = opts.ensure_installed,
+                callback = function()
+                    -- syntax highlighting, provided by Neovim
+                    vim.treesitter.start()
+                    -- folds, provided by Neovim
+                    vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+                    vim.wo.foldmethod = 'expr'
+                    -- indentation, provided by nvim-treesitter
+                    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                end,
             })
-        end
+        end,
     },
 }
