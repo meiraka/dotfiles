a9f6579093da48c5ab733d3a1478dc1b4c3b9442
meiraka <meiraka.pre@gmail.com>
2025-12-13 00:35:35 +0900

    add diagram hover

--- config/nvim/lua/plugins/lang.lua
+++ config/nvim/lua/plugins/lang.lua
@@ -43,5 +43,16 @@ return {
             })
         end,
         ft = { "markdown", "norg" },
+        keys = {
+            {
+                "K", -- or any key you prefer
+                function()
+                    require("diagram").show_diagram_hover()
+                end,
+                mode = "n",
+                ft = { "markdown", "norg" }, -- Only in these filetypes
+                desc = "Show diagram in new tab",
+            },
+        },
     }
 }
