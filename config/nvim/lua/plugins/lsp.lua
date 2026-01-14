70e2ca591d0522cfd957937db054409d26915168
meiraka <meiraka.pre@gmail.com>
2026-01-15 00:17:25 +0900

    disable cmdline preselect

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -49,6 +49,14 @@ return {
         opts = {
             keymap = { preset = 'enter' },
             completion = { documentation = { auto_show = true, } },
+            cmdline = {
+                keymap = {
+                    preset = 'cmdline',
+                    ['<Up>'] = { 'select_prev', 'fallback' },
+                    ['<Down>'] = { 'select_next', 'fallback' },
+                },
+                completion = { list = { selection = { preselect = false, auto_insert = true } } },
+            },
         },
     },
     { 'neovim/nvim-lspconfig', },
