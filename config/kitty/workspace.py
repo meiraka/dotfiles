ca1d8aa52c0d1df2c040737b14902cda49a8b4bc
meiraka <meiraka.pre@gmail.com>
2026-01-19 20:19:29 +0900

    fix doc

--- config/kitty/workspace.py
+++ config/kitty/workspace.py
@@ -7,14 +7,13 @@ from kittens.tui.handler import kitten_ui
 @kitten_ui(allow_remote_control=True)
 def main(args: list[str]) -> str:
     title = args[1]
-    # get the result of running kitten @ ls
     cp = main.remote_control(
         ['ls'], capture_output=True)
     if cp.returncode != 0:
         sys.stderr.buffer.write(cp.stderr)
         raise SystemExit(cp.returncode)
     output = json.loads(cp.stdout)
-    # open a new tab with a title specified by the user
+    # find tab which title is args[1]
     for w in output:
         for t in w["tabs"]:
             if t["title"] == title:
@@ -23,8 +22,11 @@ def main(args: list[str]) -> str:
             continue
         break
     else:
+        # if tab is not exists, create new tab
         main.remote_control(
             ['launch', '--type=tab', '--tab-title', title], check=True)
+
+    # focus
     main.remote_control(['focus-tab', '--match', 'title:'+title], check=True)
 
     return ""
