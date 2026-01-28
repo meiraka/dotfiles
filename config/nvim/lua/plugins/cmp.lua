0d6f1bbddad4c7904b719d3f50c0a62461fe27e1
meiraka <meiraka.pre@gmail.com>
2026-01-29 00:34:19 +0900

    fix src

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -112,8 +112,7 @@ return {
             cmp.setup.cmdline(':', {
                 mapping = cmp.mapping.preset.cmdline(),
                 sources = cmp.config.sources({
-                    { name = 'path' }
-                }, {
+                    { name = 'path' },
                     {
                         name = 'cmdline',
                         option = {
