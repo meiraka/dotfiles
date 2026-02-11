a717bdbd43464d02a06ed8429a01b07e0c7ef5e3
meiraka <meiraka.pre@gmail.com>
2026-02-11 18:55:20 +0900

    rm

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -231,7 +231,6 @@ return {
             { "<leader>fc",      function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
             { "<leader>ff",      function() Snacks.picker.files() end,                                   desc = "Find Files" },
             { "<leader>fg",      function() Snacks.picker.git_files() end,                               desc = "Find Git Files" },
-            { "<leader>fp",      function() Snacks.picker.projects() end,                                desc = "Projects" },
             { "<leader>fr",      function() Snacks.picker.recent() end,                                  desc = "Recent" },
             -- git
             { "<leader>gb",      function() Snacks.picker.git_branches() end,                            desc = "Git Branches" },
