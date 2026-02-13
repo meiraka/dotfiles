dc319aa579e9d524ed40a30ecde62e14a1f7eae0
meiraka <meiraka.pre@gmail.com>
2026-02-14 00:08:31 +0900

    fix desc

--- config/nvim/lua/plugins/editing.lua
+++ config/nvim/lua/plugins/editing.lua
@@ -2,14 +2,14 @@ return {
     {
         "monaqa/dial.nvim",
         keys = {
-            { "<C-a>",  function() require("dial.map").manipulate("increment", "normal") end,  help = "increment" },
-            { "<C-x>",  function() require("dial.map").manipulate("decrement", "normal") end,  help = "decrement" },
-            { "g<C-a>", function() require("dial.map").manipulate("increment", "gnormal") end, help = "increment group" },
-            { "g<C-x>", function() require("dial.map").manipulate("decrement", "gnormal") end, help = "decrement group" },
-            { "<C-a>",  function() require("dial.map").manipulate("increment", "visual") end,  help = "increment",       mode = { "x" } },
-            { "<C-x>",  function() require("dial.map").manipulate("decrement", "visual") end,  help = "decrement",       mode = { "x" } },
-            { "g<C-a>", function() require("dial.map").manipulate("increment", "gvisual") end, help = "increment group", mode = { "x" } },
-            { "g<C-x>", function() require("dial.map").manipulate("decrement", "gvisual") end, help = "decrement group", mode = { "x" } },
+            { "<C-a>",  function() require("dial.map").manipulate("increment", "normal") end,  desc = "increment" },
+            { "<C-x>",  function() require("dial.map").manipulate("decrement", "normal") end,  desc = "decrement" },
+            { "g<C-a>", function() require("dial.map").manipulate("increment", "gnormal") end, desc = "increment group" },
+            { "g<C-x>", function() require("dial.map").manipulate("decrement", "gnormal") end, desc = "decrement group" },
+            { "<C-a>",  function() require("dial.map").manipulate("increment", "visual") end,  desc = "increment",       mode = { "x" } },
+            { "<C-x>",  function() require("dial.map").manipulate("decrement", "visual") end,  desc = "decrement",       mode = { "x" } },
+            { "g<C-a>", function() require("dial.map").manipulate("increment", "gvisual") end, desc = "increment group", mode = { "x" } },
+            { "g<C-x>", function() require("dial.map").manipulate("decrement", "gvisual") end, desc = "decrement group", mode = { "x" } },
         },
     },
     {
