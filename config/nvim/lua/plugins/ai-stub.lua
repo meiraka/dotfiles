46ee012e5839e031fbdcb019af9799f440615e71
meiraka <meiraka.pre@gmail.com>
2026-04-04 21:24:00 +0900

    optional

--- config/nvim/lua/plugins/ai-stub.lua
+++ config/nvim/lua/plugins/ai-stub.lua
@@ -1,9 +1,3 @@
-for _, file in ipairs(vim.fn.readdir(vim.fs.joinpath(vim.fn.stdpath('config'), "lua", "plugins"))) do
-    if string.sub(file, 1, 2) == "ai" and file ~= "ai-stub.lua" then
-        return {}
-    end
-end
-
 return {
-    { "olimorris/codecompanion.nvim", lazy = true },
+    { "olimorris/codecompanion.nvim", lazy = true, optional = true },
 }
