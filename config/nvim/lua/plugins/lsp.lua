aa5b474d82c4c524996aa009a90b2a0cfef98898
meiraka <meiraka.pre@gmail.com>
2026-05-02 11:48:03 +0900

    move lsp keymap from mason

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,9 @@
 vim.api.nvim_create_autocmd('User', {
     pattern = 'VeryLazy',
     callback = function()
+        vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, { desc = 'LSP Rename' })
+        vim.keymap.set('n', '<leader>d', vim.lsp.buf.definition, { desc = 'LSP Definition' })
+        vim.keymap.set('n', 'K', function() vim.lsp.buf.hover({ border = 'rounded' }) end, { desc = 'LSP Hover' })
         vim.api.nvim_create_autocmd('LspAttach', {
             group = vim.api.nvim_create_augroup('my.lsp', {}),
             callback = function(args)
@@ -35,18 +38,19 @@ vim.api.nvim_create_autocmd('User', {
 return {
     {
         "mason-org/mason-lspconfig.nvim",
-        opts = {},
+        opts = {
+            ensure_installed = {
+                "gopls",
+                "lua_ls",
+                "pylsp",
+            },
+        },
         dependencies = {
             { "mason-org/mason.nvim", opts = {} },
             "neovim/nvim-lspconfig",
         },
         event = { "BufReadPre", "BufNewFile" },
         cmd = { "Mason" },
-        keys = {
-            { '<leader>r', vim.lsp.buf.rename,                                       desc = 'LSP Rename' },
-            { '<leader>d', vim.lsp.buf.definition,                                   desc = 'LSP Definition' },
-            { 'K',         function() vim.lsp.buf.hover({ border = 'rounded' }) end, desc = 'LSP Hover' },
-        },
     },
     { 'neovim/nvim-lspconfig' },
 }
